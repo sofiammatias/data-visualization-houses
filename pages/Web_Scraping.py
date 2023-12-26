7ebb326f6ec8fa9336c0566a32b5737b5b3a46f3
Sofia Matias <114782592+sofiammatias@users.noreply.github.com>
2023-12-26 17:25:48 +0000

    Cleaned code with black and mypy.

--- pages/Web_Scraping.py
+++ pages/Web_Scraping.py
@@ -25,13 +25,13 @@ retrieve updated information from the website. Save the data in csv, then load t
 st.write(f"Web scraping URL: {url}")
 
 
-def website_scraped(soup, state: bool, url: str, counter:int, num_pages:int):
+def website_page_scraped(soup, progress_text):
     """
-        Scrapes website from page "counter" to page "num_pages".
+        Scrapes one page from website, given by "soup" variable.
         Retrieves house url, house type, house price, lat, long, address, number of bedrooms, 
         parking space.
         Retrieves lat and long from each house url.
-        REtrieves the rest of info from the current page.   
+        Retrieves the rest of info from the current page.   
     """
 
     
@@ -56,78 +56,97 @@ def website_scraped(soup, state: bool, url: str, counter:int, num_pages:int):
         return values
 
     info_to_df = []
-    payload = {}
-    progress_text = f"Scraping website page {counter} of {num_pages}"
     my_bar = st.progress(0, text=progress_text)
-    for i in range(counter, num_pages):
-        contents = soup.find_all("div", {"class": "MuiGrid-root jss125 MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 MuiGrid-grid-md-3 MuiGrid-grid-lg-3"})
-        for j, content in enumerate(contents):
-            my_bar.progress(int(100 * j / len(contents)), text=progress_text)
-            content_text = content.text
-            regex = r"(.*?)R\$ ([\d.,]+)(.*?)m²(\d+)m²?(\d+)"
-            matches = re.match(regex, content_text)
-            if matches is not None:
-                result = [matches.group(i).strip() for i in range(1, 6)]
-            new_partial_link = content.find("a", {"class": "MuiButtonBase-root MuiCardActionArea-root jss266"}).get("href")
-            new_link = f"https://loft.com.br{new_partial_link}"
-            response = requests.get(new_link)
-            soup = BeautifulSoup(response.text, "html.parser")
-            json_info = json.loads(soup.find("script",{"type":"application/json"}).text)
-
-            latitude = float(json_extract(json_info, "latitude")[0])
-            longitude = float(json_extract(json_info, "longitude")[0])
-            
-            info_to_df.append(
-                dict(
-                    house_links=new_link,
-                    house_type=result[0],
-                    house_price=int(result[1].replace(".", "")),
-                    address=result[2],
-                    area=result[3],
-                    bedrooms=result[4][0],
-                    parking_space=result[4][1],
-                    latitude=latitude,
-                    longitude=longitude,
-                )
-            )
-
-        payload["pagina"] = i+1
-        progress_text = f"Scraping website page {i+1} of {num_pages}"
-        response = requests.get(url, params=payload)
+    #for i in range(counter, num_pages):
+    contents = soup.find_all("div", {"class": "MuiGrid-root jss125 MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 MuiGrid-grid-md-3 MuiGrid-grid-lg-3"})
+    for j, content in enumerate(contents):
+        my_bar.progress(int(100 * (j+1) / len(contents)), text=progress_text)
+        content_text = content.text
+        regex = r"(.*?)R\$ ([\d.,]+)(.*?)m²(\d+)m²?(\d+)"
+        matches = re.match(regex, content_text)
+        if matches is not None:
+            result = [matches.group(i).strip() for i in range(1, 6)]
+        new_partial_link = content.find("a", {"class": "MuiButtonBase-root MuiCardActionArea-root jss266"}).get("href")
+        new_link = f"https://loft.com.br{new_partial_link}"
+        response = requests.get(new_link)
         soup = BeautifulSoup(response.text, "html.parser")
-        if "Nenhum imóvel encontrado" in soup.text or state == True:
-            break
-    houses_df = pd.DataFrame(info_to_df)
-    return houses_df
+        json_info = json.loads(soup.find("script",{"type":"application/json"}).text)
 
+        latitude = float(json_extract(json_info, "latitude")[0])
+        longitude = float(json_extract(json_info, "longitude")[0])
+            
+        info_to_df.append(
+            dict(
+                house_links=new_link,
+                house_type=result[0],
+                house_price=int(result[1].replace(".", "")),
+                address=result[2],
+                area=result[3],
+                bedrooms=result[4][0],
+                parking_space=result[4][1],
+                latitude=latitude,
+                longitude=longitude,
+            )
+        )
+        houses_df = pd.DataFrame(info_to_df)
+    return houses_df
+  
 # initialize scraping loop
-houses_df = pd.DataFrame([])
+st.session_state['webscraping'] = True
 i = 0
 col1, col2 = st.columns([3,1])
 
 # buttons for web scraping and cancel web scraping
 with col1:
     if st.button("Scrape Website"):
+        # initializing web scraping: get number of pages and first page content
         st.session_state['webscraping'] = True
+        partial_houses_df = pd.DataFrame([])
+        houses_df = pd.DataFrame([])
+        st.session_state['df'] = houses_df
+        payload = {}
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
         pages = soup.find("a", {"class": "MuiTypography-root MuiLink-root jss349 MuiLink-underlineNone jss350 jss365 jss352 jss367 jss342 MuiTypography-colorPrimary"})
         num_pages = int(pages.getText())
+        progress_text = f"Scraping website page {i} of {num_pages}"
 
+        # loop to web scrape: partial_houses_df is updated with each house
         while st.session_state['webscraping'] == True or i != num_pages:
-            partial_houses_df = website_scraped(soup, st.session_state['webscraping'], url, i, num_pages)
+            partial_houses_df = website_page_scraped(soup, progress_text)
+            houses_df = pd.concat ([houses_df, partial_houses_df])
             i += 1
-        houses_df = pd.concat ([houses_df, partial_houses_df]) 
+            payload["pagina"] = i
+            progress_text = f"Scraping website page {i} of {num_pages}"
+            try:
+                response = requests.get(url, params=payload)
+            except requests.exceptions.Timeout or requests.exceptions.ConnectionError:
+                # Set up for a retry, or continue in a retry loop
+                response = requests.get(url, params=payload)
+            except requests.exceptions.TooManyRedirects:
+                # Tell the user their URL was bad and try a different one
+                st.write(f"Page {i} has a problem and cannot be read. Going for page {i+1}")
+                i += 1
+                payload["pagina"] = i
+                progress_text = f"Scraping website page {i} of {num_pages}"
+                response = requests.get(url, params=payload)
+            #except requests.exceptions.RequestException as e:
+                # catastrophic error. bail.
+                #raise SystemExit(e)
+            soup = BeautifulSoup(response.text, "html.parser")
+        if i == num_pages:
+            st.session_state['webscraping'] == False
+        
 with col2:
     if st.button("Cancel Scraping"):
         st.session_state['webscraping'] = False
 
-if st.session_state['webscraping'] == False or i == num_pages:
+if st.session_state['webscraping'] == False:
     # Save file        
     houses_df.to_csv("houses.csv", index=False)
     st.success(
               'File "houses.csv" is ready to be saved. Use the button below to save file locally.'
-            )
+                )
 
     @st.cache_data
     def convert_df(df):
